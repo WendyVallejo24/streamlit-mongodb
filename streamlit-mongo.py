bf1920e674345f9c66cbe2697bcf278683abd011
Wendy Vallejo <wbvalpat24@gmail.com>
2023-06-02 23:23:52 -0600

    update streamlit mongo

--- streamlit-mongo.py
+++ streamlit-mongo.py
@@ -7,10 +7,14 @@ from pymongo import MongoClient
 from collections import Counter
 import pandas as pd
 
-# replace here with your mongodb url 
-uri = "mongodb+srv://wendy24:wendy_ptr2402@artists.klushnh.mongodb.net/?retryWrites=true&w=majority"
+sidebar = st.sidebar
+sidebar.image('perfil.jpeg')
+sidebar.title("Wendy Belén Vallejo Patraca")
+sidebar.write("Matricula: S20006733")
+sidebar.write("zS20006733@estudiantes.uv.mx")
+sidebar.markdown("___")
 
-# Connect to meme MongoDB database
+uri = "mongodb+srv://wendy24:wendy_ptr2402@artists.klushnh.mongodb.net/?retryWrites=true&w=majority"
 
 try:
     client = MongoClient(uri, server_api=ServerApi('1'))
@@ -22,18 +26,63 @@ try:
 except:
     print("Could not connect to MongoDB")
 
-# streamlit run streamlit-mongo.py --server.enableCORS false --server.enableXsrfProtection false
-
-st.title("Visualizacion de MongoDB")
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
+st.title("Artists MongoDB Dashboard")
 @st.experimental_memo(ttl=600)
-def get_data():
+def get_dataReactions():
     items = db.artists_reactions.find()
-    items = list(items)  # make hashable for st.cache_data
+    items = list(items)
     return items
 
-items = get_data()
+def commentsDataframe():
+    db = client['artists']
+    collection = db['artists_comments']
+    documentos = collection.find()
+    datos = []
+    for documento in documentos:
+        datos.append(documento)
+    df = pd.DataFrame(datos)
+    return df
+
+def reactionsDataframe():
+    db = client['artists']
+    collection = db['artists_reactions']
+    documentos = collection.find()
+    datos = []
+    for documento in documentos:
+        datos.append(documento)
+    df = pd.DataFrame(datos)
+    return df
+datos_reactions= reactionsDataframe()
+
+def grafico_pastel_reactions(datos_reactions):
+    fig = px.pie(datos_reactions, names='reactionId')
+    st.plotly_chart(fig)
+
+datos_df = commentsDataframe()
+def grafico_barras_usuarios(datos_df):
+    fig = px.bar(datos_df.groupby('userId').count().reset_index(), x='userId', y='comment')
+    st.plotly_chart(fig)
+
+def grafico_pastel_objetos(datos_df):
+    fig = px.pie(datos_df, names='objectId')
+    st.plotly_chart(fig)
+
+def grafico_caja_usuarios(datos_df):
+    fig = px.box(datos_df, x='userId', y='comment')
+    st.plotly_chart(fig)
+
+@st.experimental_memo(ttl=600)
+def get_dataComents():
+    itemsC = db.artists_comments.find()
+    itemsC = list(itemsC)
+    return itemsC
+
+itemsC = get_dataComents()
+
+def mostrarComents():
+    listasdb = db.artists_comments.distinct("comentId")
+    listasdb = list(listasdb)
+    return listasdb
 
 def mostrar():
     listasdb = db.artists_reactions.distinct("reactionId")
@@ -43,16 +92,60 @@ def mostrar():
 print(mostrar())
 
 
-items = get_data()
+items = get_dataReactions()
 
-sidebar = st.sidebar
-sidebar.image('perfil.jpeg')
-sidebar.title("Wendy Belén Vallejo Patraca")
-sidebar.write("Matricula: S20006733")
-sidebar.write("zS20006733@estudiantes.uv.mx")
+sidebar.header("Vistas de datos")
+sidebar.header("Selecciona una opción")
+
+agree = sidebar.checkbox("Comentarios en tabla")
+if agree:
+    st.header("info de comentarios...")
+    st.dataframe(itemsC)
+    st.markdown("_")
+
+agree = sidebar.checkbox("Reacciones en tabla")
+if agree:
+    st.header("info de reacciones...")
+    st.dataframe(items)
+    st.markdown("_")
+
+agree = sidebar.checkbox("Raw de comentarios")
+if agree:
+    st.header("info de comentarios...")
+    st.write(itemsC)
+    st.markdown("_")
+#
+    
+agree = sidebar.checkbox("Raw de reacciones")
+if agree:
+    st.header("Resultados...")
+    st.write(items)
+    st.markdown("_")
+
+if st.sidebar.checkbox('Comentarios'):
+    collection = db['artists_comments']
+    collection2 = db['artists_reactions']
+    registros = collection.find()
+
+    # Crear una lista con los campos "comment" y "objectId"
+    data = [["Comentario", "Publicacion", "Usuario"]]
+    for registro in registros:
+        comment = registro['comment']
+        objectId = registro['objectId']
+        userId = registro['userId']
+        data.append([comment, objectId, userId])
+
+    # Mostrar la tabla en Streamlit
+    st.table(data)
+
 sidebar.markdown("___")
+sidebar.header("Gráficas")
+sidebar.header("Selecciona una opción")
 
-############### reactions ################
+if st.sidebar.checkbox('Grafica de pastel reactions'):
+    st.header("Gráfico de pastel para mostrar la proporción de reacciones por tipo:")
+    grafico_pastel_reactions(datos_reactions)
+
 if st.sidebar.checkbox('Grafica de barras reactions'):
 
     collection = db['artists_reactions']
@@ -75,144 +168,22 @@ if st.sidebar.checkbox('Grafica de barras reactions'):
         xaxis_title="Reaction ID",
         yaxis_title="Cantidad"
     )
-
     # Mostrar la gráfica en Streamlit
     st.plotly_chart(fig)
-    st.markdown("___")
-    
-#
-def reactionsDataframe():
-    db = client['artists']
-    collection = db['artists_reactions']
-    documentos = collection.find()
-    datos = []
-    for documento in documentos:
-        datos.append(documento)
 
-    df = pd.DataFrame(datos)
-    return df
+if st.sidebar.checkbox('Grafica de pastel comments'):
+    st.header("Gráfico de pastel para mostrar la proporción de comentarios por objeto:")
+    grafico_pastel_objetos(datos_df)
 
-datos_reactions= reactionsDataframe()
+if st.sidebar.checkbox('Grafica de barras usuarios'):
+    st.header("Gráfico de barras para contar el número de comentarios por usuario:")
+    grafico_barras_usuarios(datos_df)
 
-def grafico_pastel_reactions(datos_reactions):
-    fig = px.pie(datos_reactions, names='reactionId')
-    st.plotly_chart(fig)
-def grafico_pastel_reactions(datos_reactions):
-    fig = px.pie(datos_reactions, names='reactionId')
-    st.plotly_chart(fig)
-    
-if st.sidebar.checkbox('Grafica de pastel reactions'):
-    st.subheader("Cantidad de reactions por tipo, representado en gráfico de pastel:")
-    grafico_pastel_reactions(datos_reactions)
-    st.markdown("___")
-    
-################ comentarios ######################
-sidebar.markdown("___")
-sidebar.markdown("Comentarios")
-
-if st.sidebar.checkbox('Tabla de comentarios'):
-    st.header("Comentarios...")
-    collection = db['artists_comments']
-    registros = collection.find()
-
-    # Crear una lista con los campos "comment" y "objectId"
-    data = [["Comentario", "Publicacion", "Usuario"]]
-    for registro in registros:
-        comment = registro['comment']
-        objectId = registro['objectId']
-        userId = registro['userId']
-        data.append([comment, objectId, userId])
-
-    # Mostrar la tabla en Streamlit
-    st.table(data)
-    st.markdown("___")
-
-
-#histograma
-def commentsDataframe():
-    db = client['artists']
-    collection = db['artists_comments']
-    documentos = collection.find()
-    datos = []
-    for documento in documentos:
-        datos.append(documento)
-
-    df = pd.DataFrame(datos)
-    return df
-
-datos_df = commentsDataframe()
 
 def grafico_barras_agrupadas(datos_df):
     fig = px.bar(datos_df, x='userId', y='comment', color='objectId', barmode='group')
     st.plotly_chart(fig)
 
-if st.sidebar.checkbox('Grafica de barras comments'):
-    st.header("Cantidad de comentarios por publicacion:")
+if st.sidebar.checkbox('Grafica de barras agrupadas'):
+    st.header("Gráfico de barras agrupadas para mostrar el número de comentarios por usuario y por objeto:")
     grafico_barras_agrupadas(datos_df)
-    st.markdown("___")
-    
-def grafico_pastel_objetos(datos_df):
-    fig = px.pie(datos_df, names='objectId')
-    st.plotly_chart(fig)
-    
-if st.sidebar.checkbox('Grafica de pastel comments'):
-    st.subheader("Cantidad de comentarios por publicacion:")
-    grafico_pastel_objetos(datos_df)
-    st.markdown("___")
-    
-    
-############### usuarios #################
-sidebar.markdown("___")
-sidebar.markdown("Usuarios")
-
-if st.sidebar.checkbox('Tabla de usuarios'):
-    st.header("Usuarios...")
-    collection = db['artists_comments']
-    registros = collection.find()
-
-    # Crear una lista con los campos "comment" y "objectId"
-    data = [["Comentario", "Usuario"]]
-    for registro in registros:
-        comment = registro['comment']
-        userId = registro['userId']
-        data.append([comment, userId])
-
-    # Mostrar la tabla en Streamlit
-    st.table(data)
-    st.markdown("___")
-
-def grafico_barras_usuarios(datos_df):
-    fig = px.bar(datos_df.groupby('userId').count().reset_index(), x='userId', y='comment')
-    st.plotly_chart(fig)
-    
-if st.sidebar.checkbox('Grafica de barras usuarios'):
-    st.header("Cantidad de comentarios por usuario:")
-    grafico_barras_usuarios(datos_df)
-    st.markdown("___")
-    
-def grafico_pastel_users(datos_df):
-    fig = px.pie(datos_df, names='userId')
-    st.plotly_chart(fig)
-    
-if st.sidebar.checkbox('Grafica de pastel usuarios'):
-    st.header("Cantidad de comentarios por usuario:")
-    grafico_pastel_users(datos_df)
-    st.markdown("___")
-    
-sidebar.markdown("___")
-
-#    
-agree = sidebar.checkbox("Ver resultados raw (json) ? ")
-if agree:
-    st.header("Resultados...")
-    st.write(items)
-    st.markdown("___")
-
-sidebar.markdown("___")
-sidebar.markdown("Reacciones")
-#
-agree = sidebar.checkbox("Tabla de reactions")
-if agree:
-    st.subheader("Reactions...")
-    st.dataframe(items)
-    st.markdown("___")
